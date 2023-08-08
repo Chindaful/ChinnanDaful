a231656ab6bae0efbdd7e08f80cd86fe6c407d10
Chindaful <cdafulk@gmail.com>
2023-08-08 18:54:57 +0100

    second commit

---
+++ class12.js
@@ -0,0 +1,30 @@
+console.log("javascript " + ""+ "is cool")
+var mystring = "new to javascript"
+console.log(mystring)
+var firstname = 5
+var secondname = 3
+firstname + secondname
+var result = firstname + secondname
+console.log(result)
+var result1 = firstname - secondname
+var result2 = firstname / secondname
+var result3 = firstname * secondname
+var result4 = firstname % secondname
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(result4)
+console.log("firstname + secondname =" + " " + result)
+
+var me = 5
+me = me + 3
+me2 = 6
+me2 --
+console.log(me + 1) 
+console.log(me)
+console.log(me)
+console.log(me2 + 6)
+console.log(me2)
+mydec = 2.5
+mydec = mydec % 2
+console.log(mydec)
